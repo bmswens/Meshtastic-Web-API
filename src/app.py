a504eec3e25dc95d0031f36dbafadab38299b39a
Brandon Swenson <bmswens@gmail.com>
2022-12-23 02:41:32 -0600

    Update CI/CD pipeline

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
+# built in
+import argparse
+import json
+
 # 3rd party
 from flask import Flask
 import meshtastic.serial_interface
 
 # custom
-from api import api
+from api import api, export
 import db
 
 
@@ -16,8 +20,25 @@ def create_app():
     app.interface = interface
     app.db = db.Database("db.sqlite")
     api.init_app(app)
-    return app
+    return app, api
 
 if __name__ == "__main__": # pragma: no cover
-    app = create_app()
-    app.run()
+    parser = argparse.ArgumentParser(
+        description='Start the server or export the swagger.json'
+    )
+    parser.add_argument(
+        '--export',
+        nargs="?",
+        dest="export",
+        default=False,
+        const=True
+    )
+    args = parser.parse_args()
+    app, api = create_app()
+    if args.export:
+        app.config["SERVER_NAME"] = "http://bmswens.github.io/Meshtastic-Web-API/"
+        with app.app_context():
+            with open("swagger.json", 'w') as output:
+                output.write(json.dumps(api.__schema__, indent=2))
+    else:
+        app.run()
