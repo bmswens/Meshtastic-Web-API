617610a5da27e601cbcfe5fc49ac21a976d49c7d
Brandon Swenson <bmswens@gmail.com>
2022-12-23 02:51:57 -0600

    update for CI/CD

--- src/app.py
+++ src/app.py
@@ -11,11 +11,13 @@ from api import api
 import db
 
 
-def create_app():
+def create_app(connect=True):
     # flask init
     app = Flask(__name__)
     # meshtastic interface that all sub modules will use
-    interface = meshtastic.serial_interface.SerialInterface()
+    interface = None
+    if connect:
+        interface = meshtastic.serial_interface.SerialInterface()
     db.start()
     app.interface = interface
     app.db = db.Database("db.sqlite")
@@ -34,11 +36,12 @@ if __name__ == "__main__": # pragma: no cover
         const=True
     )
     args = parser.parse_args()
-    app, api = create_app()
     if args.export:
+        app, api = create_app(False)
         app.config["SERVER_NAME"] = "http://bmswens.github.io/Meshtastic-Web-API/"
         with app.app_context():
             with open("swagger.json", 'w') as output:
                 output.write(json.dumps(api.__schema__, indent=2))
     else:
+        app, api = create_app()
         app.run()
